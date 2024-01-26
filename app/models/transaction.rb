63a8c0c24bbe005a43b337491b17e1ecc4d2626d
Jezzelle <jezreelsonsobremonte@ymail.com>
2024-01-25 18:13:31 -0600

    Added validations and associations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,9 @@
 class Transaction < ApplicationRecord
+  # Association
   belongs_to :cryptocurrency
+
+  # Validations
+  validates :type, presence: true, inclusion: { in: %w[buy sell] }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
+
