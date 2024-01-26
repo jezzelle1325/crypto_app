bd0bed841d95335c335368e9fb6e614ba09ecc85
Jezzelle <jezreelsonsobremonte@ymail.com>
2024-01-25 18:50:52 -0600

    Added users_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_26_000713) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_26_003300) do
   create_table "cryptocurrencies", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_26_000713) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cryptocurrency_id"], name: "index_transactions_on_cryptocurrency_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "transactions", "cryptocurrencies"
