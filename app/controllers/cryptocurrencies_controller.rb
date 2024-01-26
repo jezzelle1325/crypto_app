86c8745b112e06e7ca013bfd5d4a99b252b11a83
Jezzelle <jezreelsonsobremonte@ymail.com>
2024-01-25 19:38:07 -0600

    Implemented Search

--- app/controllers/cryptocurrencies_controller.rb
+++ app/controllers/cryptocurrencies_controller.rb
@@ -1,9 +1,19 @@
 class CryptocurrenciesController < ApplicationController
+
   def index
     @cryptocurrencies = Cryptocurrency.all
+
+    if params[:search].present?
+      @cryptocurrencies = @cryptocurrencies.where('name LIKE ?', "%#{params[:search]}%")
+    end
+
+    if params[:filter].present? && params[:filter] != 'All'
+      @cryptocurrencies = @cryptocurrencies.joins(:transactions).where(transactions: { type: params[:filter] })
+    end
   end
 
   def show
     @cryptocurrency = Cryptocurrency.find(params[:id])
   end
+
 end
